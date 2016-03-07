285427966e90173d56b1768ed6823da692d4bf00
reggi <thomas@reggi.com>
2016-03-07 17:06:11 -0500

    working subscription

--- github-repos.js
+++ github-repos.js
@@ -1,12 +1,49 @@
 import axios from 'axios'
 import Rx from 'rx'
+import Promise from 'bluebird'
+import crypto from 'crypto'
+import fs from 'fs-extra'
+
+Promise.promisifyAll(fs)
+
+let {
+  ensureDir,
+  outputJson } = fs
 
 const GITHUB_USER = 'reggi'
 
-function getRepos (user){
+function getRepos (user) {
   let theUrl = `https://api.github.com/users/${user}/repos`
   return axios.get(theUrl)
+  .then(result => {
+    return result.data
+  })
 }
+
+function getHash (json) {
+  let str = JSON.stringify(json)
+  return crypto.createHmac('sha256', str)
+}
+
+function createFile (hash, data) {
+  return outputJson(`/repos/${hash}.json`, data)
+}
+
+// ensureDir('/repos')
+
+let repos = Rx.Observable.fromPromise(getRepos(GITHUB_USER))
+
+let reposSubscription = repos.subscribe(
+  function (x) {
+    console.log(x)
+    console.log('Next: %s', x)
+  },
+  function (err) {
+    console.log('Error: %s', err)
+  },
+  function () {
+    console.log('Completed')
+  })
 
 // make request to get all github repos
 // ensure a folder called "/repos" exists
