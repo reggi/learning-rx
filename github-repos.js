bc853144e0493c527dac2681c4bf91edf00f6d5a
reggi <thomas@reggi.com>
2016-03-07 16:51:27 -0500

    instructions

--- github-repos.js
+++ github-repos.js
@@ -7,3 +7,8 @@ function getRepos (user){
   let theUrl = `https://api.github.com/users/${user}/repos`
   return axios.get(theUrl)
 }
+
+// make request to get all github repos
+// ensure a folder called "/repos" exists
+// get the hash of the repo data
+// save individual data as a json file where the name of the file is the hash
